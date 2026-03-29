6656c870d7254ffa2535afaa7d7f9f5309db42ec
Mekjah <believeinsomething2421@gmail.com>
2026-03-29 07:58:24 +0100

    Update

--- components/ui/UpdateBanner.tsx
+++ components/ui/UpdateBanner.tsx
@@ -22,8 +22,8 @@ export function UpdateBanner() {
     localStorage.setItem('uzima-update-dismissed', 'true');
   };
 
- const isVisible = true;
-  // const isVisible = updateAvailable && !dismissed;
+ 
+  const isVisible = updateAvailable && !dismissed;
 
   useEffect(() => {
     const updateCssVar = () => {
